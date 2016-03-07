d6c26eae4177b86d57d42cd1ebb45d9522639fac
MazoonBanu <mazoon1@yandex.com>
2016-03-07 13:50:06 +0530

    Added files via upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mazoon::Application.config.secret_key_base = 'b4d7a3a458239a98cec2faa0ac6301fe302042d4e66dacbaa255def113d646f8d6fabf3cb4199a138e86d329c433e9b8192c29d39071ba6700a5bd365f0d7b8a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mazoon::Application.config.session_store :cookie_store, key: '_mazoon_session'
